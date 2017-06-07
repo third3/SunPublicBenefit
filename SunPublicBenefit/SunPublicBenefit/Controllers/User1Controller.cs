97bae8e46ea03d2b39be0fb6821c93171f669ee0
zero <zero@ZEROC29C>
2017-06-07 17:42:14 +0800

    母版页

---
+++ SunPublicBenefit/SunPublicBenefit/Controllers/User1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SunPublicBenefit.Models;
+
+namespace SunPublicBenefit.Controllers
+{
+    public class User1Controller : Controller
+    {
+        private SunShineContainer db = new SunShineContainer();
+
+        // GET: User1
+        public ActionResult Index()
+        {
+            return View(db.User1Set.ToList());
+        }
+
+        // GET: User1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User1 user1 = db.User1Set.Find(id);
+            if (user1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user1);
+        }
+
+        // GET: User1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User1/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserName,UserPassword")] User1 user1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User1Set.Add(user1);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user1);
+        }
+
+        // GET: User1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User1 user1 = db.User1Set.Find(id);
+            if (user1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user1);
+        }
+
+        // POST: User1/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserName,UserPassword")] User1 user1)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user1).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user1);
+        }
+
+        // GET: User1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User1 user1 = db.User1Set.Find(id);
+            if (user1 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user1);
+        }
+
+        // POST: User1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User1 user1 = db.User1Set.Find(id);
+            db.User1Set.Remove(user1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
