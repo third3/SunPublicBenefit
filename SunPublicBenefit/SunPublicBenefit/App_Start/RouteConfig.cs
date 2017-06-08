9bca1cdf5ca22dc0fd3185a81e1ae1eb644b2874
zero <zero@ZEROC29C>
2017-06-08 08:17:27 +0800

    home Index

--- SunPublicBenefit/SunPublicBenefit/App_Start/RouteConfig.cs
+++ SunPublicBenefit/SunPublicBenefit/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SunPublicBenefit
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Default", action = "Home", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
