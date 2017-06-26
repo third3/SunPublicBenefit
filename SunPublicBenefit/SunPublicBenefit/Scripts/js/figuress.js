a342ccb425ee3c7168baaee91835b20796849396
大肚翔 <@DESKTOP-D782PN8>
2017-06-26 18:40:23 +0800

    1

---
+++ SunPublicBenefit/SunPublicBenefit/Scripts/js/figuress.js
@@ -0,0 +1,16 @@
+﻿//(function ($) {
+//    $.fn.simplePage = function (o) {
+//        var options = {
+//            pager: '.pager',//表格控件容器
+//            container: '.pb_figures_ms',//放置表格数据的容器
+//            form: '#form',//放置查询条件的表单
+//            pageForm: '#pageForm',//放置隐藏与的DIV
+//            url: '',//发送请求的地址
+//            currentPage: 1,
+//            pageSize: 2,
+//            type: null,//可选:action
+//            pageShow:7
+//        }
+//        return sth;
+//    }
+//})(jQuery)
