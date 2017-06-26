c89907741b6dbffde96e9af7e613fda4d07a5bc3
大肚翔 <@DESKTOP-D782PN8>
2017-06-26 23:24:35 +0800

    1

--- SunPublicBenefit/SunPublicBenefit/Scripts/js/figuress.js
+++ SunPublicBenefit/SunPublicBenefit/Scripts/js/figuress.js
@@ -14,3 +14,17 @@
 //        return sth;
 //    }
 //})(jQuery)
+$.ajax({
+    type: "GET",
+    url: "/persons.txt",
+    dataType: "json",
+    success: function (data) {
+        var totalPage = Math.ceil(data.length / 4);
+        for (var i = 0; i < 4; i++) {
+            $("#pf_ct").append("<div class='pb_figures_ms'><div class='pb_figures_ms_ig'><a href=''><img src='../images/cy_ytuz.jpg' /></a></div><div class='pb_figures_ms_pr'><ul><li class='pfmp_tl'><a href=''>" + data[i].personsTitle + "</a></li><li class='pfmp_ms'><span>" + data[i].personsMessage + "</span></li><li class='pfmp_lb'><span>01月08日</span><span>Time</span><span>标签:<a>公益人物</a></span></li><li class='pfmp_details'><a href=''>查看详情</a></li></ul></div>");
+        }
+        for (var j = 1; j <= totalPage; j++) {
+            $(".pageBar ul li.reset").append("<a href=''>" + j + "</a>");
+        }
+    }
+});
